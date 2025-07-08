c1b3ad3842b7aec2aaa1739c2ff81a8e8db6e9d0
Cachume <albertq703@gmail.com>
2025-07-07 21:52:19 -0400

    Modify Header

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,10 +2,16 @@ const btnmenu = document.getElementById("btn-menu")
 const btnclose = document.getElementById("btn-close")
 const menu = document.getElementById("sub-menu-container")
 const submenu = document.getElementById("sub-menu")
+const user_img = document.getElementById("user-img")
 
 btnmenu.addEventListener("click", function(){
     menu.classList.toggle("active")
 })
 btnclose.addEventListener("click", function(){
     menu.classList.toggle("active")
+})
+
+
+user_img.addEventListener("click",function(){
+    document.getElementById("user-options").classList.toggle("active")
 })
