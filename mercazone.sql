f04e6a2e4c7a8cb6e72c9efc08166c8879296135
Cachume <albertq703@gmail.com>
2025-11-21 13:14:25 -0400

    bd1

--- mercazone.sql
+++ mercazone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2025 a las 13:44:27
+-- Tiempo de generación: 21-11-2025 a las 18:11:43
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
